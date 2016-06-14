7fb550d814c77db5e954dfef38283a801b1b56b2
vitaefelus <aloufino@gmail.com>
2016-06-14 21:59:56 +0200

    skoncze to jutro

--- assets/js/lighthouse.js
+++ assets/js/lighthouse.js
@@ -17,33 +17,33 @@ function Lighthouse() {
 
               lighthouse.traverse(function (child) {
                   if (child instanceof THREE.Mesh) {
-                      //console.log("mesh " + child.id);
+                      console.log("mesh " + child.id);
                       ///ZAKOMENTOWAŁAM NA RAZIE, BO MULI JAK OSTATNI SKURWYSYN
-                      //266 nieoteksturowany!!!!!
+                      //396 nieoteksturowany!!!!!
                       //glowna kolumna (marmur jasny)
-                    /*  if (child.id == 222) {
-                          child.material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('assets/textures/lighthouseTextures/18_BaseColor.jpg') });
+                      if (child.id == 352) {
+                        child.material = new THREE.MeshPhongMaterial({ color: 0xeae8dc });
                       }
                       //drewno
-                      if (child.id == 224 || child.id == 226 || child.id == 228 || child.id == 232 || child.id == 240 || child.id == 242 || child.id == 246 || child.id == 248 || child.id == 254 || child.id == 272) {
-                          child.material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('assets/textures/lighthouseTextures/8_BaseColor.jpg') });
+                      if (child.id == 354 || child.id == 410 ||child.id == 408 || child.id == 356 || child.id == 358 || child.id == 362 || child.id == 370 || child.id == 372 || child.id == 376 || child.id == 378 || child.id == 384 || child.id == 402) {
+                        child.material = new THREE.MeshPhongMaterial({ color: 0xad8658 });
                       }
                       //czerwony
-                      if (child.id == 230 || child.id == 234 || child.id == 252 || child.id == 260 || child.id == 268 || child.id == 274) {
-                          child.material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('assets/textures/lighthouseTextures/19_BaseColor.jpg') });
+                      if (child.id == 360 || child.id == 364 || child.id == 382 || child.id == 390 || child.id == 398 || child.id == 404) {
+                        child.material = new THREE.MeshPhongMaterial({ color: 0xbe1f1f });
                       }
                       //stal
-                      if (child.id == 236 || child.id == 238 || child.id == 250 || child.id == 256 || child.id == 276) {
-                          child.material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('assets/textures/lighthouseTextures/stal.jpg') });
+                      if (child.id == 366 || child.id == 368 || child.id == 380 || child.id == 386 || child.id == 406) {
+                        child.material = new THREE.MeshPhongMaterial({ color: 0x8f8f8f });
                       }
                       //ciemny marmur
-                      if (child.id == 244 || child.id == 258 || child.id == 262 || child.id == 264) {
-                          child.material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('assets/textures/lighthouseTextures/2_BaseColor.jpg') });
+                      if (child.id == 374 || child.id == 388 || child.id == 392 || child.id == 394) {
+                        child.material = new THREE.MeshPhongMaterial({ color: 0x3a3a3a });
                       }
-                      if (child.id == 278) {
-                          child.material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+                      if (child.id == 412) {
+                          child.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
                           //child.material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('assets/js/modelTextures/lighthouseTextures/stal.jpg') });
-                      }*/
+                      }
                   }
 
               });
